071baf3283dc9c249fca53b16919b0f47038403e
SaKu <sakuofficial@gmail.com>
2020-11-12 13:45:08 +0530

    Added data add method

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,29 @@ var router = express.Router();
 var connection = require('../config/connection');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
-  connection.query('SELECT * FROM users', function(err,rows) {
+  connection.query('SELECT * FROM users', function (err, rows) {
     if (err) throw err;
-    res.render('index', {users:rows});
+    res.render('index', {
+      users: rows
+    });
+  });
+
+});
+
+router.post('/addUser', function (req, res) {
+
+  const userdata = {
+    fname: req.body.fname,
+    lname: req.body.lname,
+    email: req.body.email,
+    profession: req.body.profession
+  }
+  connection.query("INSERT INTO users set ?", userdata, function (err, result) {
+    if(err) throw err;
+    res.redirect('/');
+    console.log("Data Inserted!");
   });
 
 });
